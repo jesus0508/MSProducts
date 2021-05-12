62884068364cea4e20d098b385b4466c548e84f0
Jesus Alarcon <jesus.leo0508@gmail.com>
2021-05-12 12:15:41 -0500

    first commit

---
+++ create-projects.bash
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+mkdir microservices
+cd microservices
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=product-service \
+--package-name=se.magnus.microservices.core.product \
+--groupId=se.magnus.microservices.core.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-service
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=review-service \
+--package-name=se.magnus.microservices.core.review \
+--groupId=se.magnus.microservices.core.review \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+review-service
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=recommendation-service \
+--package-name=se.magnus.microservices.core.recommendation \
+--groupId=se.magnus.microservices.core.recommendation \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+recommendation-service
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=product-composite-service \
+--package-name=se.magnus.microservices.composite.product \
+--groupId=se.magnus.microservices.composite.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-composite-service
+
+cd ..
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=api \
+--package-name=se.magnus.api \
+--groupId=se.magnus.microservices.api \
+--dependencies=webflux \
+--version=1.0.0-SNAPSHOT \
+api
+
+spring init \
+--boot-version=2.4.5 \
+--build=gradle \
+--java-version=11 \
+--packaging=jar \
+--name=util \
+--package-name=se.magnus.util \
+--groupId=se.magnus.microservices.util \
+--dependencies=webflux \
+--version=1.0.0-SNAPSHOT \
+util
